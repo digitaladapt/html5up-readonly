220e8fc8743666ae55a98e7933b54716c2aec8e3
Andrew Stowell <andrew@digitaladapt.com>
2024-05-06 14:50:34 +0000

    added a few bash scripts to make it easier to get up and running.

---
+++ _preview.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# will cause port 4000 to be available for reverse proxy to call on
+# but will *NOT* update the live website, because we are using a different destination
+bundle exec jekyll serve --destination preview
+
